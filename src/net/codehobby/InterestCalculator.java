1a505ac7296399e3c348c55a4d0ead0c26c6c8d4
luigi1015 <github@314ghz.com>
2014-01-17 10:17:18 -0600

    Finished some of the interest calculation code.

--- src/net/codehobby/InterestCalculator.java
+++ src/net/codehobby/InterestCalculator.java
@@ -1,31 +1,44 @@
 package net.codehobby;
 
+import java.math.BigDecimal;
+
 public class InterestCalculator
 {
-	private int initialDeposit;//The deposit at the opening of the account.
-	private int percentInterest;//The percent interest gained on the account every cycle. The value is as a percent. The assumption is that the interest is the same every cycle to simplify the calculations.
-	private int recurringDeposit;//The deposit every cycle. It's assumed that the same amount is deposited every cycle to simplify the calculations and record-keeping.
+	private BigDecimal initialDeposit;//The deposit at the opening of the account.
+	private BigDecimal percentInterest;//The percent interest gained on the account every cycle. The value is as a percent. The assumption is that the interest is the same every cycle to simplify the calculations.
+	private BigDecimal recurringDeposit;//The deposit every cycle. It's assumed that the same amount is deposited every cycle to simplify the calculations and record-keeping.
 
-	public InterestCalculator( int newInitialDeposit, int newPercentInterest )
+	public InterestCalculator( BigDecimal newInitialDeposit, BigDecimal newPercentInterest )
 	{//Constructor with only the initial deposit and the interest rate. Assumes there is no recurring deposit.
 		initialDeposit = newInitialDeposit;
 		percentInterest = newPercentInterest;
-		recurringDeposit = 0;
+		recurringDeposit = new BigDecimal( "0" );
+		initialDeposit.setScale( 2, BigDecimal.ROUND_HALF_UP );
+		percentInterest.setScale( 2, BigDecimal.ROUND_HALF_UP );
+		recurringDeposit.setScale( 2, BigDecimal.ROUND_HALF_UP );
 	}
 
-	public InterestCalculator( int newInitialDeposit, int newPercentInterest, int newRecurringDeposit )
+	public InterestCalculator( BigDecimal newInitialDeposit, BigDecimal newPercentInterest, BigDecimal newRecurringDeposit )
 	{//Constructor with the initial deposit, the interest rate and the recurring deposit.
 		initialDeposit = newInitialDeposit;
 		percentInterest = newPercentInterest;
 		recurringDeposit = newRecurringDeposit;
+		initialDeposit.setScale( 2, BigDecimal.ROUND_HALF_UP );
+		percentInterest.setScale( 2, BigDecimal.ROUND_HALF_UP );
+		recurringDeposit.setScale( 2, BigDecimal.ROUND_HALF_UP );
 	}
 	
-	public int calculateValue( int cycle )
+	public BigDecimal calculateValue( int cycle )
 	{//Calculates the value after cycle cycles and returns that.
-		int value = 0;
+		BigDecimal value = new BigDecimal( "0" );//The accumulated value up to the accumulated cycle.
+		BigDecimal interest;//The interest accumulated this cycle.
+		value.setScale( 2, BigDecimal.ROUND_HALF_UP );
+		value = value.add( initialDeposit );
+
 		for( int i = 0; i < cycle; i++ )
 		{
-			//Finish this part later. Probably should rethink the variable type. Maybe change to BigDecimal.
+			interest = value.multiply( percentInterest.divide( new BigDecimal("100") ) );
+			value = value.add( interest );
 		}
 		return value;
 	}
